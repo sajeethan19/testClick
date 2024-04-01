4dfccc7c66b634e8ba3e08d1bdfada238c5592d2
Sajeethan Gulendran <73322508+sajeethan19@users.noreply.github.com>
2024-04-01 14:46:02 +0530

    cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,18 @@ import $ from 'jquery';
 
 function App() {
   $(document).ready(function () {
-    // Disabling right-click context menu
     $(document).bind("contextmenu", function (e) {
         e.preventDefault();
-        // alert("Right click is disabled on this website");
     });
 
-    // Disabling F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+Shift+C
     $(document).on("keydown", function (e) {
-        if (e.keyCode == 123) { // F12
+        if (e.keyCode == 123) {
             e.preventDefault();
-            // alert("Opening Developer Tools is disabled on this website");
         }
         if ((e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || // Ctrl+Shift+I
             (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || // Ctrl+Shift+J
             (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))) { // Ctrl+Shift+C
             e.preventDefault();
-            // alert("Opening Developer Tools is disabled on this website");
         }
     });
 });
